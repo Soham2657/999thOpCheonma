f26d4851157eebed35069c1253e9507926750d25
Soham Bhattacharyya <cse23100@cemk.ac.in>
2026-02-12 00:49:57 +0530

    email issue again

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -3,10 +3,18 @@ const nodemailer=require("nodemailer");
 //transporter used by sendEmail.js
 
 const transporter=nodemailer.createTransport({
-    service:"gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth:{
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASS,
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 20000,
+    tls: {
+        rejectUnauthorized: false,
     },
 });
 module.exports=transporter;
