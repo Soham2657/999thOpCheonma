108f41389d251439406a342172b27470749878a5
Soham Bhattacharyya <cse23100@cemk.ac.in>
2026-02-11 19:56:23 +0530

    added frontend url to cors origin

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-   origin: "http://localhost:5173",
+   origin: "https://999th-op-cheonma.vercel.app",
   credentials: true
 }));
 app.use(express.json());
